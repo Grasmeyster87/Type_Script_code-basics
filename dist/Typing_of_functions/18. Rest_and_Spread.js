dd86b945fe55d36871f6f80b3724199fe0c271eb
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-03 13:25:18 +0200

    18. Rest и Spread 2

--- dist/Typing_of_functions/18. Rest_and_Spread.js
+++ dist/Typing_of_functions/18. Rest_and_Spread.js
@@ -1,1 +1,29 @@
+var __spreadArray = (this && this.__spreadArray) || function (to, from, pack) {
+    if (pack || arguments.length === 2) for (var i = 0, l = from.length, ar; i < l; i++) {
+        if (ar || !(i in from)) {
+            if (!ar) ar = Array.prototype.slice.call(from, 0, i);
+            ar[i] = from[i];
+        }
+    }
+    return to.concat(ar || Array.prototype.slice.call(from));
+};
+function max18() {
+    var numbers = [];
+    for (var _i = 0; _i < arguments.length; _i++) {
+        numbers[_i] = arguments[_i];
+    }
+    return Math.max.apply(Math, numbers);
+}
+var numbers18 = [1, 2, 3];
+Math.max.apply(Math, numbers18);
+var args = [1, 2];
+function max(value) {
+    var numbers = [];
+    for (var _i = 1; _i < arguments.length; _i++) {
+        numbers[_i - 1] = arguments[_i];
+    }
+    return Math.max.apply(Math, __spreadArray([value], numbers, false));
+}
+max(1, 2, 3);
+max(234);
 //# sourceMappingURL=18.%20Rest_and_Spread.js.map
