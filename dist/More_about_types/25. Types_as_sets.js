5bbd11bf1da5e23b97ce30d5a3482f4b14d793e6
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-05 23:05:25 +0200

    25. Types_as_sets

--- dist/More_about_types/25. Types_as_sets.js
+++ dist/More_about_types/25. Types_as_sets.js
@@ -1,1 +1,4 @@
+var c25_1 = 11;
+var c25_2 = null;
+var c25_3 = undefined;
 //# sourceMappingURL=25.%20Types_as_sets.js.map
---
+++ src/More_about_types/25. Types_as_sets.ts
@@ -0,0 +1,14 @@
+
+// Типы как множества
+/*
+type SomeType = number | string;
+const v1: SomeType = 1;
+const v2: SomeType = 'hexlet';
+*/
+
+type CustomType = number | null | undefined;
+
+const c25_1: CustomType = 11;
+const c25_2: CustomType = null;
+const c25_3: CustomType = undefined;
+
