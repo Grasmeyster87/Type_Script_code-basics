9fd1563e10f5843f35a47f3e12fefad26e202f8b
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-06 13:15:18 +0200

    26. Union_Types

--- dist/More_about_types/26. Объединения (Union Types).js
+++ dist/More_about_types/26. Объединения (Union Types).js
@@ -1,1 +1,21 @@
+var val = 10;
+val = 'My string';
+var concat26 = function (base, suffix) { return "".concat(base).concat(suffix); };
+function lastIndex(str, char) {
+    var val4 = '';
+    str.toString()
+        .split('')
+        .forEach(function (element, index) {
+        if (element === char) {
+            return (val4 = index);
+        }
+        else if (index == str.toString().split('').length - 1) {
+            val4 = null;
+        }
+    });
+    return val4;
+}
+var str = 'test';
+lastIndex(str, 't');
+lastIndex(str, 'p');
 //# sourceMappingURL=26.%20%D0%9E%D0%B1%D1%8A%D0%B5%D0%B4%D0%B8%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20(Union%20Types).js.map
---
+++ src/More_about_types/26. Объединения (Union Types).ts
@@ -0,0 +1,53 @@
+// Объединения (Union Types)
+
+type at = (str: string, position: number) => string | undefined;
+
+/*
+В TypeScript это означает, что в результате мы получаем тип, 
+который обещает содержать переменную одного из типов объединения. 
+Так мы можем завести свой тип, под который попадают все строки ИЛИ числа:
+*/
+
+type NumberOrString = number | string;
+
+let val: NumberOrString = 10;
+val = 'My string';
+// val - true; // Type 'boolean' is not assignable to type 'NUmber Or String';
+
+/*
+
+На практике нередко встречаются случаи, когда нам нужно поддержать работу функции с большим количеством допустимых значений.
+ В JavaScript мы можем соединить строку не только со строкой, но и числом или с true. 
+ Для решения похожей задачи в прошлых уроках мы познакомились с перегрузкой функции. 
+ Опишем тип такой функции с применением объединения:
+
+*/
+
+type AllowedToConcatenation = number | string | null | undefined | boolean;
+
+const concat26 = (
+    base: AllowedToConcatenation,
+    suffix: AllowedToConcatenation
+): string => `${base}${suffix}`;
+
+function lastIndex(
+    str: AllowedToConcatenation,
+    char: AllowedToConcatenation
+): AllowedToConcatenation {
+    let val4: AllowedToConcatenation = '';
+    str.toString()
+        .split('')
+        .forEach((element, index) => {
+            if (element === char) {
+                return (val4 = index);
+            } else if (index == str.toString().split('').length - 1) {
+                val4 = null;
+            }
+        });
+
+    return val4;
+}
+
+const str = 'test';
+lastIndex(str, 't'); // 3
+lastIndex(str, 'p'); // null
