ddb8f1f4fa462cfb63973a3c7f088f4faad0cb5f
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-07 14:40:53 +0200

    34 Classes

--- dist/Classes/34. Classes.js
+++ dist/Classes/34. Classes.js
@@ -1,1 +1,14 @@
+var CustomFile = (function () {
+    function CustomFile(_a) {
+        var name = _a.name, size = _a.size;
+        this.name = name;
+        this.size = size;
+    }
+    CustomFile.prototype.toString = function () {
+        return "".concat(this.name, " (").concat(this.size, " bytes)");
+    };
+    return CustomFile;
+}());
+var file = new CustomFile({ name: 'open-world.jpeg', size: 1000 });
+console.log(file.toString());
 //# sourceMappingURL=34.%20Classes.js.map
---
+++ src/Classes/34. Classes.ts
@@ -0,0 +1,61 @@
+// Классы
+/*
+class Point34 {
+    x: number;
+    y: number;
+  
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+  }
+  
+  const p = new Point34(10, 20);
+  console.log(p); // { x: 10, y: 20 }
+*/
+// Поле класса
+/*
+  class Point341 {
+    x: number;
+  
+    y: number;
+  
+    // Возвращаемый тип не указывается, так как это конструктор
+    constructor(x: number, y: number) {
+      this.x = x;
+      this.y = y;
+    }
+  
+    get inspect(): string {
+      return `(${this.x}, ${this.y})`
+    }
+  }
+  
+  const p = new Point341(2, 5);
+  // Свойство есть, а поля такого нет
+  console.log(p.inspect); // (2, 5)*/
+
+  /*# Задание
+
+  Реализуйте класс CustomFile, в конструктор которого передается объект с полями: name — именем файла, и size — размером в байтах. Внутри класса определите метод toString(), который должен вернуть форматированную строку в формате <file-name> (<size> bytes).
+*/
+type CustomFileOptions = {
+    name: string;
+    size: number;
+  };
+  class CustomFile {
+    name: string;
+    size: number;
+
+     constructor({ name, size }: CustomFileOptions) {
+      this.name = name;
+      this.size = size;
+    }
+  
+    toString() {
+      return `${this.name} (${this.size} bytes)`
+    }
+  }
+  
+  const file = new CustomFile({ name: 'open-world.jpeg', size: 1000 });
+  console.log(file.toString()); // open-world.jpeg (1000 bytes)
