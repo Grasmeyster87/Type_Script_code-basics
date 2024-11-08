911e9eed99a6b8b29ac2a5cae1454bc27432d20e
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-08 18:26:06 +0200

    43. mplementation_of_interfaces_by_classes

--- dist/Interfaces/43. Implementation_of_interfaces_by_classes.js
+++ dist/Interfaces/43. Implementation_of_interfaces_by_classes.js
@@ -1,1 +1,30 @@
+var Phonebook = (function () {
+    function Phonebook() {
+        this.data = {};
+    }
+    Object.defineProperty(Phonebook.prototype, "entries", {
+        get: function () {
+            return this.data;
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Phonebook.prototype.get = function (key) {
+        if (this.data[key]) {
+            return this.data[key];
+        }
+        else {
+            return null;
+        }
+    };
+    ;
+    Phonebook.prototype.set = function (key, value) {
+        this.data[key] = value;
+    };
+    ;
+    return Phonebook;
+}());
+Примеры: var myNote = new Phonebook();
+myNote.set('help', 911);
+myNote.get('help');
 //# sourceMappingURL=43.%20Implementation_of_interfaces_by_classes.js.map
