f6de9a2759b7d584e765c564f9ae66e9fbb5e249
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-11 13:24:41 +0200

    46. Generics_(Functions)

--- dist/Generics/46. Generics_(Functions).js
+++ dist/Generics/46. Generics_(Functions).js
@@ -1,1 +1,7 @@
+var coll;
+coll.push(1);
+coll.push(10);
+coll.push(99);
+var newColl = coll.filter(function (value) { return value % 2 == 0; });
+console.log(newColl.items);
 //# sourceMappingURL=46.%20Generics_(Functions).js.map
