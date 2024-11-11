4f07ed87e629e7a7d06997472ad0114ddfa6ca8f
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-11 15:39:38 +0200

    50. Type_object

--- dist/Typing_of_objects/50. Type_object.js
+++ dist/Typing_of_objects/50. Type_object.js
@@ -1,1 +1,29 @@
+function toString(obj) {
+    return obj.toString();
+}
+toString('wow');
+toString(123);
+toString({});
+function toString(obj) {
+    return obj.toString();
+}
+toString('wow');
+toString(123);
+toString({});
+function extract(object, keys) {
+    return keys.reduce(function (accumulator, key) {
+        if (Object.prototype.hasOwnProperty.call(object, key)) {
+            accumulator[key] = object[key];
+        }
+        return accumulator;
+    }, {});
+}
+var user50 = {
+    name: 'Tirion',
+    email: 'tirion@lanister.got',
+    age: 35,
+};
+console.log(
+extract(user50, ['name', 'age'])
+);
 //# sourceMappingURL=50.%20Type_object.js.map
---
+++ src/Typing_of_objects/50. Type_object.ts
@@ -0,0 +1,62 @@
+
+// Тип object
+/*
+Иногда нам нужно ограничить входной параметр функции типом «любой объект». 
+Например, это нужно для функции, которая проверяет наличие ключей в объекте. 
+Существует несколько способов сделать такую проверку, но не все из них работают как ожидается.
+
+Посмотрим на пример:
+*/
+
+// В качестве типа используется {}
+function toString(obj: {}) {
+  return obj.toString();
+}
+
+toString('wow'); // Ok!
+toString(123); // Ok!
+toString({}); // Ok!
+
+/*
+Пустой объектный тип {} подразумевает под собой объект любой структуры и ограничивает множество всех значений 
+
+за исключением null и undefined. Пустой интерфейс работает так же, как и пустой объектный тип. Это не то, что мы ожидали.
+
+Тип Object — это тип объекта. Он работает так же, как тип {} с некоторыми отличиями. 
+
+Он предопределяет типы некоторых встроенных методов, например, toString(), а тип {} этого не делает. Например:
+
+Второе определение bar не работает, потому что тип Object указывает на то, что метод toString() должен возвращать строку.
+
+Если мы хотим работать с непримитивными значениями, то для этого существует еще один тип object (с маленькой буквы):
+*/
+function toString(obj: object) {
+  return obj.toString();
+}
+
+toString('wow'); // Error!
+toString(123); // Error!
+toString({}); // Ok!
+
+/*
+Задание
+
+Реализуйте функцию extract(object, keys), которая возвращает новый объект c указанными ключами. Например:
+*/
+
+function extract<T extends object, K extends keyof T>(object: T, keys: K[]): Pick<T, K> {
+    return keys.reduce((accumulator, key) => {
+      if (Object.prototype.hasOwnProperty.call(object, key)) {
+        accumulator[key] = object[key];
+      }
+      return accumulator;
+    }, {} as Pick<T, K>);
+  }
+
+const user50 = {
+  name: 'Tirion',
+  email: 'tirion@lanister.got',
+  age: 35,
+}
+
+extract(user50, ['name', 'age']); // { name: 'Tirion', age: 35 }
