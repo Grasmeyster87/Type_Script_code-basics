11afdf05f3855a6dd4107191bd1dbc459c6c3203
Grasmeyster87 <109178000+Grasmeyster87@users.noreply.github.com>
2024-11-11 15:18:31 +0200

    49. Generics_on_Classes

--- dist/Generics/49. Generics_on_Classes.js
+++ dist/Generics/49. Generics_on_Classes.js
@@ -1,1 +1,20 @@
+var Queue = (function () {
+    function Queue() {
+        this.data = [];
+    }
+    Queue.prototype.enqueue = function (element) {
+        this.data.push(element);
+    };
+    Queue.prototype.dequeue = function () {
+        if (this.data.length === 0) {
+            throw new Error('Queue is empty');
+        }
+        return this.data.shift();
+    };
+    return Queue;
+}());
+var queue = new Queue();
+queue.enqueue(1);
+queue.dequeue();
+queue.dequeue();
 //# sourceMappingURL=49.%20Generics_on_Classes.js.map
